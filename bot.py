48eec53d8c01febaa0fcf194bb2afdcbf5e5955e
Oluwatobi Ekundayo <tobiekundayo07@gmail.com>
2024-11-12 02:53:17 +0100

    Chnaged disk path

--- bot.py
+++ bot.py
@@ -30,8 +30,8 @@ import os
 
 load_dotenv()
 DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-WALLET_DATA_FILE = "./data/wallet_tracking_data.json"
-USER_WALLET_FILE = "./data/user_wallet_data_file.json"
+WALLET_DATA_FILE = "/data/wallet_tracking_data.json"
+USER_WALLET_FILE = "/data/user_wallet_data_file.json"
 
 ua = UserAgent()
 intents = discord.Intents.default()
@@ -61,7 +61,7 @@ async def add_server(interaction: discord.Interaction, server_id: str):
 
     # Load the allowed servers from JSON
     try:
-        with open("./data/allowed_servers.json", "r") as f:
+        with open("/data/allowed_servers.json", "r") as f:
             data = json.load(f)
     except FileNotFoundError:
         # The file does not exist, continue with the initialized data
@@ -78,7 +78,7 @@ async def add_server(interaction: discord.Interaction, server_id: str):
     allowed_servers.append(server_id)
     data["allowed_servers"] = allowed_servers
 
-    with open("./data/allowed_servers.json", "w") as f:
+    with open("/data/allowed_servers.json", "w") as f:
         json.dump(data, f, indent=4)
 
     await interaction.response.send_message(f"Server {server_id} has been successfully added to the allowed list.", ephemeral=True)
@@ -88,7 +88,7 @@ async def add_server(interaction: discord.Interaction, server_id: str):
 async def remove_server(interaction: discord.Interaction, server_id: str):
     # Load the allowed servers from JSON
     try:
-        with open("./data/allowed_servers.json", "r") as f:
+        with open("/data/allowed_servers.json", "r") as f:
             data = json.load(f)
             allowed_servers = data.get("allowed_servers", [])
     except FileNotFoundError:
@@ -103,7 +103,7 @@ async def remove_server(interaction: discord.Interaction, server_id: str):
     allowed_servers.remove(server_id)
     data["allowed_servers"] = allowed_servers
 
-    with open("./data/allowed_servers.json", "w") as f:
+    with open("/data/allowed_servers.json", "w") as f:
         json.dump(data, f, indent=4)
 
     await interaction.response.send_message(f"Server {server_id} has been successfully removed from the allowed list.", ephemeral=True)
@@ -115,7 +115,7 @@ def allowed_server_only():
         async def wrapper(interaction: discord.Interaction, *args, **kwargs):
             # Load allowed servers from JSON
             try:
-                with open("./data/allowed_servers.json", "r") as f:
+                with open("/data/allowed_servers.json", "r") as f:
                     data = json.load(f)
                     allowed_servers = data.get("allowed_servers", [])
             except FileNotFoundError:
@@ -1567,8 +1567,8 @@ All of the code below, until the point marked end represents the rune tracking l
 def load_runes_data():
     """Load the runes data from a JSON file if it exists."""
     global data
-    if os.path.exists("./data/runes_mint_data.json"):
-        with open("./data/runes_mint_data.json", "r") as f:
+    if os.path.exists("/data/runes_mint_data.json"):
+        with open("/data/runes_mint_data.json", "r") as f:
             data = json.load(f)
     else:
         # Do not initialize any structure here to prevent auto-creation of the file
@@ -1577,7 +1577,7 @@ def load_runes_data():
 def save_data():
     # Only save guilds that have at least one tracking channel configured
     data_to_save = {guild_id: channels for guild_id, channels in data.items() if channels}
-    with open("./data./data.json", "w") as f:
+    with open("/data/data.json", "w") as f:
         json.dump(data_to_save, f, indent=4)
 
 def get_last_sent_percentage(guild_id, rune_id):
